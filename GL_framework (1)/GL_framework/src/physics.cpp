6f9c581d13943adfa20eb9cd9e7ed9cf2da0c2f2
NorthSanta <xavidevesa@hotmail.com>
2017-03-11 14:52:33 +0100

    capsual parameters

--- GL_framework (1)/GL_framework/src/physics.cpp
+++ GL_framework (1)/GL_framework/src/physics.cpp
@@ -12,7 +12,18 @@ float life = 1;
 int solver;
 float elastic = 0;
 float friction = 0;
+
+
 float sphereRad = 1;
+float sphereX = 0;
+float sphereY = 1;
+float sphereZ = 0;
+
+float capsuleRad = 1;
+float capsuleX = -4;
+float capsuleY = 2;
+float capsuleZ = 2;
+
 int fontOrWaterfall;
 int changedFont;
 int changedSolver;
@@ -57,12 +68,18 @@ namespace Sphere {
 	extern void updateSphere(glm::vec3 pos, float radius = 1.f);
 	extern void drawSphere();
 }
-
+namespace Capsule {
+	extern void setupCapsule(glm::vec3 posA = glm::vec3(-3.f, 2.f, -2.f), glm::vec3 posB = glm::vec3(-4.f, 2.f, 2.f), float radius = 1.f);
+	extern void cleanupCapsule();
+	extern void updateCapsule(glm::vec3 posA, glm::vec3 posB, float radius = 1.f);
+	extern void drawCapsule();
+}
 Particle* totalParts;
 float *partVerts;
 
 void GUI() {
-	Sphere::updateSphere(glm::vec3(0.f, 1.f, 0.f), sphereRad);
+	Sphere::updateSphere(glm::vec3(sphereX, sphereY, sphereZ), sphereRad);
+	Capsule::updateCapsule(glm::vec3(capsuleX, capsuleY, capsuleZ), glm::vec3(-3.f, 2.f, -2.f), capsuleRad);
 	{	
 		
 			
@@ -74,22 +91,38 @@ void GUI() {
 		//Euler & Verlet Buttons
 		ImGui::RadioButton("Euler Solver", &solver, 0); ImGui::SameLine();
 		ImGui::RadioButton("Verlet Solver", &solver, 1);
-
+		ImGui::Separator();
 		ImGui::Text("Emitter Type:");
 		//Font or Waterfall	
 		ImGui::RadioButton("Font", &fontOrWaterfall, 0); ImGui::SameLine();
 		ImGui::RadioButton("Waterfall", &fontOrWaterfall, 1);
+		ImGui::Separator();
 		//Emission & life Sliders
+		ImGui::Text("Particle Parameters:");
 		ImGui::SliderInt("Emission Rate", &emm, 100, 10000);
 		ImGui::SliderFloat("Life Expectancy", &life, 1, 5);
-		
+		ImGui::Separator();
 		//Elastic & Friccion Inputs
-		ImGui::SliderFloat("Elastic Coeficient", &elastic, 0, 1);
+		ImGui::Text("Elastic & Friction Coeficients:");
+		ImGui::SliderFloat("Elastic", &elastic, 0, 1);
 		//ImGui::InputFloat("Elastic Coeficient", &elastic);
-		ImGui::InputFloat("Friction Coeficient", &friction);
+		ImGui::SliderFloat("Friction", &friction,0,1);
 		//Sphere Position & radius
+		ImGui::Separator();
+		ImGui::Text("Sphere Parameters:");
 		ImGui::SliderFloat("Sphere Radius", &sphereRad, 0, 5);
-		//ImGui::InputFloat("Sphere Radius", &sphereRad);
+		ImGui::InputFloat("Sphere PositionX", &sphereX); 
+		ImGui::InputFloat("Sphere PositionY", &sphereY); 
+		ImGui::InputFloat("Sphere PositionZ", &sphereZ);
+		//capsule Position & radius
+		ImGui::Separator();
+		ImGui::Text("Capsule Parameters:");
+		ImGui::SliderFloat("Capsule Radius", &capsuleRad, 0, 5);
+		ImGui::InputFloat("Capsule PositionX", &capsuleX);
+		ImGui::InputFloat("Capsule PositionY", &capsuleY);
+		ImGui::InputFloat("Capsule PositionZ", &capsuleZ);
+		
+		
 		
 	}
 
@@ -269,7 +302,7 @@ void PhysicsUpdate(float dt) {
 					totalParts[i].velocity.z = -totalParts[i].velocity.z*elastic;
 				}
 
-				if (glm::distance(glm::vec3(0.f, 1.f, 0.f), totalParts[i].pos) - sphereRad <= 0) {
+				if (glm::distance(glm::vec3(sphereX, sphereY, sphereZ), totalParts[i].pos) - sphereRad <= 0) {
 					totalParts[i].velocity.y = -totalParts[i].velocity.y*elastic;
 					totalParts[i].velocity.x += totalParts[i].velocity.x;
 					totalParts[i].velocity.z += totalParts[i].velocity.z;
@@ -333,7 +366,7 @@ void PhysicsUpdate(float dt) {
 					//totalParts[i].velocity.x += totalParts[i].velocity.x;
 					//totalParts[i].velocity.z += totalParts[i].velocity.z;
 				}
-				if (glm::distance(glm::vec3(0.f, 1.f, 0.f), totalParts[i].pos) - sphereRad <= 0) {
+				if (glm::distance(glm::vec3(sphereX, sphereY, sphereZ), totalParts[i].pos) - sphereRad <= 0) {
 					//printf("XOCA");
 					totalParts[i].velocity.y = -totalParts[i].velocity.y*elastic;
 
@@ -386,10 +419,10 @@ void PhysicsUpdate(float dt) {
 					totalParts[i].pos.y = -totalParts[i].pos.y*elastic;
 				}
 				if (totalParts[i].pos.x <= -5 || totalParts[i].pos.x >= 5) {
-					totalParts[i].pos.x = -totalParts[i].pos.x;
+					totalParts[i].pos.x -= totalParts[i].pos.x - totalParts[i].antPos.x;
 				}
 				if (totalParts[i].pos.z <= -5 || totalParts[i].pos.z >= 5) {
-					totalParts[i].pos.z = -totalParts[i].pos.z;
+					totalParts[i].pos.z -= totalParts[i].pos.z - totalParts[i].antPos.z;
 				}
 /*
 				if (glm::distance(glm::vec3(0.f, 1.f, 0.f), totalParts[i].pos) - sphereRad <= 0) {
@@ -440,7 +473,15 @@ void PhysicsUpdate(float dt) {
 				totalParts[i].pos.x = totalParts[i].pos.x + (totalParts[i].pos.x - totalParts[i].antPos.x) + (totalParts[i].Forces.x / mass)*(dt*dt);
 				totalParts[i].pos.y = totalParts[i].pos.y + (totalParts[i].pos.y - totalParts[i].antPos.y) + (totalParts[i].Forces.y/mass)*(dt*dt);
 				totalParts[i].pos.z = totalParts[i].pos.z + (totalParts[i].pos.z - totalParts[i].antPos.z) + (totalParts[i].Forces.z / mass)*(dt*dt);
-
+				if (totalParts[i].pos.y <= 0.25 || totalParts[i].pos.y >= 10) {
+					totalParts[i].pos.y = -totalParts[i].pos.y*elastic;
+				}
+				if (totalParts[i].pos.x <= -5 || totalParts[i].pos.x >= 5) {
+					totalParts[i].pos.x -= totalParts[i].pos.x - totalParts[i].antPos.x;
+				}
+				if (totalParts[i].pos.z <= -5 || totalParts[i].pos.z >= 5) {
+					totalParts[i].pos.z -= totalParts[i].pos.z - totalParts[i].antPos.z;
+				}
 				totalParts[i].antPos = temp;
 
 				partVerts[i * 3 + 0] = totalParts[i].pos.x;
